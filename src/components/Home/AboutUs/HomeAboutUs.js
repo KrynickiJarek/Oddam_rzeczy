7baba17ccaf0ad55bd5f47114a9cc502aa4eea9f
KrynickiJarek <krynickijarek@gmail.com>
2020-11-23 19:58:24 +0100

    layout - about us section

--- src/components/Home/AboutUs/HomeAboutUs.js
+++ src/components/Home/AboutUs/HomeAboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../../../scss/_homeaboutus.scss";
 import decoration from "../../../assets/Decoration.svg"
+import signature from "../../../assets/Signature.svg"
 
 const HomeAboutUs = () => {
 
@@ -9,8 +10,13 @@ const HomeAboutUs = () => {
             <div className="aboutUs_content">
                 <h2 className="content_tittle">O nas</h2>
                 <img src={decoration} alt="decoration" className=" content_decoration"/>
+                <p className="content_text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts
+                    black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <img src={signature} alt="signature" className=" content_signature"/>
             </div>
             <div className="aboutUs_image"/>
+
+
         </div>
 
     );
