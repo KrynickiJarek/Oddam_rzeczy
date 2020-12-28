92326045d654211fb8303fafc5f69a36c5641664
KrynickiJarek <krynickijarek@gmail.com>
2020-12-29 00:21:19 +0100

    netlify fix

--- src/components/Home/WhoWeHelp/Pagination/Pagination.js
+++ src/components/Home/WhoWeHelp/Pagination/Pagination.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../../../../scss/_homewhowehelp.scss";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 
 const Pagination = ({postPerPage, totalPost, paginate, active}) => {
     const pageNumbers = [];
